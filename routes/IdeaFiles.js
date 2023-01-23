d15fb1246c39d8946e2019c858858011b16bd0b6
Kaushal Agrawal <kaushal.agrawal@nagarro.com>
2023-01-23 22:22:20 +0530

    Latest Commit]

--- routes/IdeaFiles.js
+++ routes/IdeaFiles.js
@@ -13,28 +13,43 @@ const storage = multer.diskStorage({
     }
 });
 
-const upload = multer({storage}).array('file');
+const upload = multer({ storage }).array('file', 10);
+
 
 router.post('/', (req, res) => {
+    debugger
+
     upload(req, res, (err) => {
-        if (!req.file) {
-            
+        console.log("Request:" + req.body.idea_id);
+        console.log("Request Crtd_By:" + req.body.crtd_by);
+        console.log("Request Crtd_Date:" + req.body.crtd_dt);
+        if (!req.files) {
+
             console.log('File is not uploaded');
         }
         console.log("file is uploaded");
+        res.statusMessage = 'Success';
+        console.log(res.statusMessage);
         console.log(req.files);
-        const sql = "INSERT INTO `ideaFiles`(`fileName`) VALUES ('" + req.files.map((value)=>value.originalname) + "')";
-       //var sql = "INSERT INTO `ideaFiles`(`fileName`) VALUES ? ('" + req.files.map((value)=>value.originalname) + "')";
-            db.query(sql,(err,result)=>{
-                if(!err){
-                    res.statusMessage = 'Success';
-                    res.status(200).send({ resultData: result });
-                    console.log(result);
-                    console.log(res.statusMessage);
-                }
-                else 
-                 console.log(err);
-            })
+        const data = {
+            fileName: req.files.map((value) => value.originalname),
+            idea_id: req.body.idea_id,
+            crtd_by: req.body.crtd_by,
+            crtd_dt: req.body.crtd_dt
+
+        }
+        console.log(data);
+        var sql = "INSERT INTO `ideafiles`(`fileName`,`idea_Id`,`crtd_by`,`crtd_dt`) VALUES  ('" + req.files.map((value) => value.originalname).join(', ') + "','" + req.body.idea_id + "','" + req.body.crtd_by + "','" + req.body.crtd_dt + "')";
+        db.query(sql, (err, result) => {
+            if (!err) {
+                res.statusMessage = 'Success';
+                res.status(200).send({ resultData: result });
+                console.log(result);
+                console.log(res.statusMessage);
+            }
+            else
+                console.log(err);
+        })
     })
 });
 
--- routes/ideas.js
+++ routes/ideas.js
@@ -1,10 +1,18 @@
 const router = require('express').Router();
+const { DATE } = require('mysql/lib/protocol/constants/types');
 const db = require('../database');
-
+const moment = require('moment');
 
 //Creating GET Router to fetch all the learner details from the MySQL Database
 router.get('/', (req, res) => {
-    db.query('SELECT * FROM ideas', (err, rows, fields) => {
+//     const date = moment(db.query('select crtd_time from ideas')
+//    ); // This is your query from MySQL
+    
+//     let mydate = date.format("YYYY-MM-DD"); 
+    
+     var date = db.query('select crtd_time from ideas')
+    var d = new Date(Date.UTC(date[0],date[1]-1,date[2]))
+    db.query('select id, email_address,ideatitle,ideator,LOB,Application, estimate,problemstatement,proposedSolution,benefits,businessvalues,comments, DATE_FORMAT(crtd_time,\'%Y-%m-%d\') AS crtd_time,crtd_by,Updtd_time,Updtd_by from ideas', (err, rows, fields) => {
         if (!err) {
             res.statusMessage = 'Success';
             res.status(200).send({ resultData: rows });
@@ -31,9 +39,9 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
     let ideaData = req.body;
-    var sql = "SET @_id = ?;SET @_email_address = ?;SET @_ideatitle = ?;SET @_ideator = ?; SET @_application = ?;SET @_status = ?;SET @_problemstatement = ?;SET @_proposedSolution = ?;SET @_benefits = ?;SET @_businessvalues = ?;SET @_comments = ?;SET @_Crtd_time = ?;SET @_Crtd_by = ?;SET @_Updtd_time = ?;SET @_Updtd_by = ?;\
-        CALL ideaAddOrEdit(@_id,@_email_address,@_ideatitle, @_ideator,@_application,@_status,@_problemstatement,@_proposedSolution,@_benefits,@_businessvalues,@_comments,@_Crtd_time,@_Crtd_by,@_Updtd_time,@_Updtd_by);";
-    db.query(sql, [ideaData.id, ideaData.email, ideaData.ideatitle, ideaData.ideator, ideaData.Application,
+    var sql = "SET @_id = ?;SET @_email_address = ?;SET @_ideatitle = ?;SET @_ideator = ?;SET @_lob = ?; SET @_application = ?;SET @_status = ?;SET @_problemstatement = ?;SET @_proposedSolution = ?;SET @_benefits = ?;SET @_businessvalues = ?;SET @_comments = ?;SET @_Crtd_time = ?;SET @_Crtd_by = ?;SET @_Updtd_time = ?;SET @_Updtd_by = ?;\
+        CALL ideaAddOrEdit(@_id,@_email_address,@_ideatitle, @_ideator,@_lob,@_application,@_status,@_problemstatement,@_proposedSolution,@_benefits,@_businessvalues,@_comments,@_Crtd_time,@_Crtd_by,@_Updtd_time,@_Updtd_by);";
+    db.query(sql, [ideaData.id, ideaData.email, ideaData.ideatitle, ideaData.ideator, ideaData.lob,ideaData.Application,
     ideaData.status, ideaData.problemstatement, ideaData.proposedSolution, ideaData.benefits,
     ideaData.businessvalues, ideaData.comments,
     ideaData.crtd_time, ideaData.Crtd_by, ideaData.Updtd_time, ideaData.Updtd_by], (err, rows, fields) => {
@@ -53,9 +61,9 @@ router.post('/', (req, res) => {
 
 router.put('/', (req, res) => {
     let ideaData = req.body;
-    var sql = "SET @_id = ?;SET @_email_address = ?;SET @_ideatitle = ?;SET @_ideator = ?; SET @_application = ?;SET @_status = ?;SET @_problemstatement = ?;SET @_proposedSolution = ?;SET @_benefits = ?;SET @_businessvalues = ?;SET @_comments = ?;SET @_Crtd_time = ?;SET @_Crtd_by = ?;SET @_Updtd_time = ?;SET @_Updtd_by = ?;\
-            CALL ideaAddOrEdit(@_id,@_email_address,@_ideatitle, @_ideator,@_application,@_status,@_problemstatement,@_proposedSolution,@_benefits,@_businessvalues,@_comments,@_Crtd_time,@_Crtd_by,@_Updtd_time,@_Updtd_by);";
-    db.query(sql, [ideaData.id, ideaData.email, ideaData.ideatitle, ideaData.ideator, ideaData.Application,
+    var sql = "SET @_id = ?;SET @_email_address = ?;SET @_ideatitle = ?;SET @_ideator = ?;SET @_lob = ?; SET @_application = ?;SET @_status = ?;SET @_problemstatement = ?;SET @_proposedSolution = ?;SET @_benefits = ?;SET @_businessvalues = ?;SET @_comments = ?;SET @_Crtd_time = ?;SET @_Crtd_by = ?;SET @_Updtd_time = ?;SET @_Updtd_by = ?;\
+            CALL ideaAddOrEdit(@_id,@_email_address,@_ideatitle, @_ideator,@_lob,@_application,@_status,@_problemstatement,@_proposedSolution,@_benefits,@_businessvalues,@_comments,@_Crtd_time,@_Crtd_by,@_Updtd_time,@_Updtd_by);";
+    db.query(sql, [ideaData.id, ideaData.email, ideaData.ideatitle, ideaData.ideator,ideaData.lob,ideaData.Application,
     ideaData.status, ideaData.problemstatement, ideaData.proposedSolution, ideaData.benefits,
     ideaData.businessvalues, ideaData.comments,
     ideaData.crtd_time, ideaData.Crtd_by, ideaData.Updtd_time, ideaData.Updtd_by], (err, rows, fields) => {
